397df1007e3c9bf8e5a8276631694e4634a87394
Andriy Romanov <mgm.agro04@gmail.com>
2025-03-11 01:38:52 +0200

    dealing with errors

--- js/gallery.js
+++ js/gallery.js
@@ -68,44 +68,45 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
-//We find the HTML element <ul class="gallery">, to which we will add images.
-const galleryContainer = document.querySelector(`.gallery`);
-/**.map(...) — iterate through the images array and generate HTML markup for each object.
-({ preview, original, description }) => ... — use object destructuring to get its properties.
-<li class="gallery-item">...</li> — each gallery item contains:
-<a class="gallery-link"> — a link (to make the image clickable).
-<img> — the image itself:
-src="${preview}" — shows a small image.
-data-source="${original}" — stores a link to the large image.
-alt="${description}" — a text description (for accessibility).
-.join('') — join all elements into a string (without a separator). */
-const galleryMarkup = images
-  .map(
-    ({ preview, original, description }) =>
-      `<li class="gallery-item">
-    <a class="gallery-link" href="${original}">
-      <img
-        class="gallery-image"
-        src="${preview}"
-        data-sourse="${original}"
-        alt="${description}"
-        width="360px"
-      />
-    </a>
-  </li>`
-  )
-  .join("");
-galleryContainer.innerHTML = galleryMarkup; //We insert the generated HTML markup into the gallery container.
+
+// Знаходимо HTML-елемент <ul class="gallery">
+const galleryContainer = document.querySelector(".gallery");
+
+// Функція для створення HTML-розмітки
+function createMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+// Додаємо розмітку у список
+galleryContainer.insertAdjacentHTML("beforeend", createMarkup(images));
+
+// Додаємо обробник подій для відкриття модального вікна
 galleryContainer.addEventListener("click", (event) => {
   event.preventDefault();
-  if (!event.target.classList.contains(`galery-image`)) {
+
+  if (!event.target.classList.contains("gallery-image")) {
     return;
   }
-  const largeImageURL = event.target.dataset.sourse;
-  //create a modal window using the basicLightbox library.
-  //Insert an <img> tag with the largeImageURL into it.
-  const instance = basicLightbox.create(
-    `<img src="${largeImageURL}" width="1112px" height="640px" top="28px" left="164px">`
-  );
-  instance.show(); //opens a modal window with a large image.
+
+  const largeImageURL = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}">
+  `);
+
+  instance.show();
 });
